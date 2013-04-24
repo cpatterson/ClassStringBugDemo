4e810bea36b6783308090126bef69487a8e4f293
Chris Patterson <chris.s.patterson@gmail.com>
2013-04-24 16:20:35 -0400

    Initial checkin.

---
+++ ClassStringBugDemo/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  ClassStringBugDemo
+//
+//  Created by Chris Patterson on 4/24/13.
+//  Copyright (c) 2013 Chris Patterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassStringBugDemo/CPAppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  CPAppDelegate.m
+//  ClassStringBugDemo
+//
+//  Created by Chris Patterson on 4/24/13.
+//  Copyright (c) 2013 Chris Patterson. All rights reserved.
+//
+
+#import "CPAppDelegate.h"
+
+@implementation CPAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+	
+	// The following line causes clang to "hang" (spin forever) trying to compile this file,
+	// when it should cause some kind of "type mismatch" compile error.
+	// The correct way to write this line is:
+	// if ([NSStringFromClass([application class]) isEqual:@"UIApplication"])
+	if ([application class] != @"UIApplication")
+		NSLog(@"App class is not UIApplication");
+	
+    return YES;
+}
+
+@end
